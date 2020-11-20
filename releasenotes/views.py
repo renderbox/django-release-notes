5394ee5b88053641b4228a6ef075d4ba34bb2273
Grant Viklund <grant.viklund@gmail.com>
2020-11-20 15:41:50 -0800

    Adding fixtures for unit tests to test views

--- releasenotes/views.py
+++ releasenotes/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, DetailView, ListView
+from django.views.generic import TemplateView, DetailView
 
 from .models import Project
 
@@ -6,16 +6,8 @@ class ReleaseNotesIndexView(TemplateView):
     template_name = "releasenotes/index.html"
 
 
-# class ReleaseNotesListView(ListView):
-#     """
-#     All releases for the project
-#     """
-#     queryset = Release.objects.filter(project__slug="trainingcamp")
-
-
 class ReleaseNotesProjectView(DetailView):
     """
     If a specific release is not provided, default to the current release
     """
     model = Project
-    # queryset = Release.objects.filter(project__slug="training-camp", state=Release.ReleaseState.CURRENT)
