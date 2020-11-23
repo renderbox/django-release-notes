efaa3ff5ad74343436cfb3a380a15872681ed187
Grant Viklund <grant.viklund@gmail.com>
2020-11-23 11:46:26 -0800

    Added M2R to the docs requirements

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
             'm2r',
         ],
         'docs': [                           # Packages needed to generate docs
+            'm2r',
             'django_extensions',
             'coverage',
             'Sphinx',
