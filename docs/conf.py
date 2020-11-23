6bb2e71302d5df536e03841eaa530d3d7ea342ca
Grant Viklund <grant.viklund@gmail.com>
2020-11-23 11:59:02 -0800

    Updated to use RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ release = version = VERSION
 # ones.
 extensions = [
     'recommonmark',
+    'sphinx_rtd_theme',
 ]
 
 # source_suffix = ['.rst', '.md']
@@ -61,7 +62,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
